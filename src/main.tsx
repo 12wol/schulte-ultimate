5c27d74383d256960e239a434ea8773a6067d1f8
12wol <18011203375@163.com>
2026-08-21 09:34:39 +0800

    Revert "Add soft garden theme switcher with pastel UI and page motions."
    This reverts commit b8a64553423706b4c8beace225271f4d8a7fd75b.

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'animal-island-ui/style'
 import './index.css'
-import './theme/soft.css'
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
-import { ThemeProvider } from './theme/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
